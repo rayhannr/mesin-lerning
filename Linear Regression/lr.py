fc16151fab900016f89b208f6184e3931e3d612b
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2020-02-21 17:44:58 +0700

    add more plot

--- Linear Regression/lr.py
+++ Linear Regression/lr.py
@@ -39,17 +39,27 @@ learningRate = 0.001
 iterations = 10000
 thetas0, thetas1, J_history = gradientDescent(X, y, np.zeros(2), learningRate, iterations)
 
+#Plotting cost function history
 plt.plot(np.arange(iterations), J_history)
 plt.xlabel('Iterations')
 plt.ylabel('Cost function history')
 plt.show()
 
+#Plotting theta's transformation per iteration
 plt.plot(thetas0, np.arange(iterations), color = 'blue')
 plt.plot(thetas1, np.arange(iterations), color = 'red')
 plt.xlabel('Thetas')
 plt.ylabel('Iterations')
 plt.show()
 
+#Plotting the regression model
 plt.scatter(X[:, 1], y, c='black', s=25)
 plt.plot(X[:, 1], thetas0[-1] + thetas1[-1] * X[:, 1], c='blue')
+plt.show()
+
+#Plotting the connection between theta and cost function
+plt.plot(thetas0, J_history)
+plt.plot(thetas1, J_history)
+plt.xlabel('Thetas')
+plt.ylabel('Cost function')
 plt.show()
